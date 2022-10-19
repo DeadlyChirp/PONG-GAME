ae7169f54bfcadade8f1094355bb92015377361c
goncalve <theo.goncalves@etu.u-paris.fr>
2022-10-19 22:33:37 +0200

    Ajout des Racket C & D

--- src/main/java/gui/GameView.java
+++ src/main/java/gui/GameView.java
@@ -6,16 +6,19 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
 import model.Court;
+import model.Feature;
 
 public class GameView {
     // class parameters
     private final Court court;
+    private Feature f;
+    private Esthetique e;
     private final Pane gameRoot; // main node of the game
     private final double scale;
     private final double xMargin = 50.0, racketThickness = 10.0; // pixels
 
     // children of the game main node
-    private final Rectangle racketA, racketB;
+    private final Rectangle racketA, racketB, racketC, racketD;
     private final Circle ball;
 
     /**
@@ -27,6 +30,9 @@ public class GameView {
         this.court = court;
         this.gameRoot = root;
         this.scale = scale;
+        this.f = new Feature();
+        this.e = new Esthetique();
+        f.setDeuxcontredeux();
 
         root.setMinWidth(court.getWidth() * scale + 2 * xMargin);
         root.setMinHeight(court.getHeight() * scale);
@@ -46,6 +52,28 @@ public class GameView {
 
         racketB.setX(court.getWidth() * scale + xMargin);
         racketB.setY(court.getRacketB() * scale);
+       
+       if (f.getDeuxcontredeux()) {
+		    racketC = new Rectangle();
+		    racketC.setWidth(court.getRacketSize() * scale);
+		    racketC.setHeight(racketThickness);
+		    racketC.setFill(Color.RED);
+		
+		    racketC.setX(500);
+		    racketC.setY(300);
+		    
+		    racketD = new Rectangle();
+		    racketD.setWidth(court.getRacketSize() * scale);
+		    racketD.setHeight(racketThickness);
+		    racketD.setFill(e.getColor());
+		
+		    racketD.setX(500);
+		    racketD.setY(0);
+		    
+       }else {
+        	racketC = new Rectangle();
+        	racketD = new Rectangle();
+       }
 
         ball = new Circle();
         ball.setRadius(court.getBallRadius());
@@ -54,9 +82,7 @@ public class GameView {
         ball.setCenterX(court.getBallX() * scale + xMargin);
         ball.setCenterY(court.getBallY() * scale);
 
-        gameRoot.getChildren().addAll(racketA, racketB, ball);
-
-
+        gameRoot.getChildren().addAll(racketA, racketB, racketC, racketD, ball);
     }
 
     public void animate() {
@@ -73,6 +99,8 @@ public class GameView {
                 last = now;
                 racketA.setY(court.getRacketA() * scale);
                 racketB.setY(court.getRacketB() * scale);
+                racketC.setY(court.getRacketC() * scale);
+                racketD.setY(court.getRacketD() * scale);
                 ball.setCenterX(court.getBallX() * scale + xMargin);
                 ball.setCenterY(court.getBallY() * scale);
             }
