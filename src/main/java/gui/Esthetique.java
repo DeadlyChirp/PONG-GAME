8363e1689af9afda40b538b95206c8b3272c7813
goncalve <theo.goncalves@etu.u-paris.fr>
2022-10-19 22:28:31 +0200

    Le fichier pour gérer l'esthetique des raquettes

---
+++ src/main/java/gui/Esthetique.java
@@ -0,0 +1,12 @@
+package gui;
+
+import javafx.scene.paint.Color;
+
+public class Esthetique{
+	
+	private static Color c = Color.LIGHTBLUE;
+	
+	public void setColor(Color c) {this.c = c;}
+	public Color getColor() { return c;}
+	
+}
